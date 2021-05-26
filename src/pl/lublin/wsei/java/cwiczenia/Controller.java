a87283d51720963846ba32d0ecf78d35f86da74f
Dawid Sałek <dawidsalekst@gmail.com>
2021-05-26 20:30:37 +0200

    ListView i jego działanie

--- src/pl/lublin/wsei/java/cwiczenia/Controller.java
+++ src/pl/lublin/wsei/java/cwiczenia/Controller.java
@@ -1,4 +1,39 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.fxml.FXML;
+import javafx.stage.FileChooser;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.ListView;
+
+import java.io.File;
+
 public class Controller {
+    public Label lbFile;
+    FileChooser fileChooser = new FileChooser();
+    FileChooser.ExtensionFilter xmlFilter = new FileChooser.ExtensionFilter("Pliki XML(*.xml)", "*.xml");
+
+    public ListView lstInfografiki;
+    ObservableList<String> tytuly = FXCollections.observableArrayList();
+    GusInfoGraphicList igList;
+
+    @FXML
+    public void initialize(){
+        fileChooser.getExtensionFilters().add(xmlFilter);
+    }
+
+    public void btnOpenFileAction(ActionEvent actionEvent) {
+        File file = fileChooser.showOpenDialog(null);
+
+        if(file!=null){
+            igList = new GusInfoGraphicList(file.getAbsolutePath());
+            lbFile.setText(file.getAbsolutePath());
+            for (Infografika ig: igList.infografiki) tytuly.add(ig.tytul);
+            lstInfografiki.setItems(tytuly);
+        } else {
+            lbFile.setText("Proszę wczytać plik...");
+        }
+    }
 }
