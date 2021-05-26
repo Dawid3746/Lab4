cb150414779197491d9aa071f40d41a0fb212cf2
Dawid Sałek <dawidsalekst@gmail.com>
2021-05-26 20:45:10 +0200

    Komponenty Button, label, ImageView i TextField oraz obsługa zmiany zaznaczonego tytułu

--- src/pl/lublin/wsei/java/cwiczenia/Controller.java
+++ src/pl/lublin/wsei/java/cwiczenia/Controller.java
@@ -1,12 +1,19 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.fxml.FXML;
 import javafx.stage.FileChooser;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 import java.io.File;
 
@@ -19,9 +26,30 @@ public class Controller {
     ObservableList<String> tytuly = FXCollections.observableArrayList();
     GusInfoGraphicList igList;
 
+    public TextField txtAdresStrony;
+    public Button btnPrzejdzDoStrony;
+    public Button btnPokazInfografike;
+    public ImageView imgMiniaturka;
+
     @FXML
     public void initialize(){
         fileChooser.getExtensionFilters().add(xmlFilter);
+        lstInfografiki.getSelectionModel().selectedIndexProperty().addListener(
+                new ChangeListener<Number>() {
+                    @Override
+                    public void changed(ObservableValue<? extends Number> observableValue, Number old_val, Number new_val){
+                        int index = new_val.intValue();
+                        if(index != -1){
+                            txtAdresStrony.setText(igList.infografiki.get(index).adresStrony);
+                            Image image = new Image(igList.infografiki.get(index).adresMiniaturki);
+                            imgMiniaturka.setImage(image);
+                        } else {
+                            txtAdresStrony.setText("");
+                            imgMiniaturka.setImage(null);
+                        }
+                    }
+                }
+        );
     }
 
     public void btnOpenFileAction(ActionEvent actionEvent) {
@@ -35,5 +63,8 @@ public class Controller {
         } else {
             lbFile.setText("Proszę wczytać plik...");
         }
+    }
+
+    public void btnZaladuj(ActionEvent actionEvent) {
     }
 }
