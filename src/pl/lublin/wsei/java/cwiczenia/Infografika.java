c456b87b80c19a9824938cf3351c13e2ad3db065
Dawid Sałek <dawidsalekst@gmail.com>
2021-05-26 19:40:38 +0200

    Konstruktor klasy Infografika oraz klasa TestInfografika

--- src/pl/lublin/wsei/java/cwiczenia/Infografika.java
+++ src/pl/lublin/wsei/java/cwiczenia/Infografika.java
@@ -1,5 +1,9 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
 public class Infografika {
     public String tytul;
     public String adresStrony;
@@ -8,7 +12,56 @@ public class Infografika {
     public String szerokosc;
     public String wysokosc;
 
-    Infografika(String tekst){
+    public Infografika(String tekst){
+
+        Pattern pat = Pattern.compile("<title><!\\[CDATA\\[(.*)\\]\\]");
+        Matcher m = pat.matcher(tekst);
+        if(m.find()){
+            tytul = m.group(1);
+        } else{
+            tytul = "";
+        }
+
+        pat = Pattern.compile("<link>(.*)<\\/link>");
+        m = pat.matcher(tekst);
+        if(m.find()){
+            adresStrony = m.group(1);
+        } else{
+            adresStrony = "";
+        }
+
+        pat = Pattern.compile("url=\\\"(.*)\\\" type");
+        m = pat.matcher(tekst);
+        if(m.find()){
+            adresGrafiki = m.group(1);
+        } else{
+            adresGrafiki = "";
+        }
+
+        pat = Pattern.compile("width=\\\"(.*)\\\" height");
+        m = pat.matcher(tekst);
+        if(m.find()){
+            szerokosc = m.group(1);
+        } else{
+            szerokosc = "";
+        }
+
+        pat = Pattern.compile("height=\\\"(.*)\\\">");
+        m = pat.matcher(tekst);
+        if(m.find()){
+            wysokosc = m.group(1);
+        } else{
+            wysokosc = "";
+        }
+
+        pat = Pattern.compile("url=\\\"(.*)\\\" \\/>");
+        m = pat.matcher(tekst);
+        if(m.find()){
+            adresMiniaturki = m.group(1);
+        } else{
+            adresMiniaturki = "";
+        }
+
 
     }
 }
